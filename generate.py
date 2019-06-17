f3f13598c4cdbb370b23237a46a09fddf094a9b5
Yuki Koyama <yuki@koyama.xyz>
2019-06-17 13:07:12 -0600

    Update the script so that it uses the template

--- generate.py
+++ generate.py
@@ -2,15 +2,18 @@ import pandas as pd
 
 def generate_dom(row):
     dom = ""
-    dom += "<h2>" + row["title"] + "</h2>\n"
+    dom += "<h5>" + row["title"] + "</h5>\n"
     dom += "<p>" + row["authors"] + "</p>\n"
     return dom
 
 if __name__ == "__main__":
     data = pd.read_csv("data.csv")
+    template = open('template.html').read()
 
     dom = ""
     for index, row in data.iterrows():
         dom += generate_dom(row)
 
-    print(dom)
+    page = template.replace("<!--list-->", dom)
+
+    print(page)
